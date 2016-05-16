fef22c0b31ab77947b27724c227bc48f82eb284d
Jairus Bondoc <jairus@e27.co>
2016-05-16 19:10:57 +0800

    updated sql

--- pronos_foot.sql
+++ pronos_foot.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2016 at 11:23 AM
+-- Generation Time: May 16, 2016 at 01:10 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -16,6 +16,59 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `pronos_foot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `id` int(2) NOT NULL,
+  `name` varchar(256) COLLATE utf8_bin NOT NULL,
+  `deleted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE IF NOT EXISTS `matches` (
+  `id` int(2) NOT NULL,
+  `team1` int(2) NOT NULL,
+  `team2` int(2) NOT NULL,
+  `datetime` datetime NOT NULL,
+  `category` varchar(256) COLLATE utf8_bin NOT NULL,
+  `deleted` tinyint(1) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `matches`
+--
+
+INSERT INTO `matches` (`id`, `team1`, `team2`, `datetime`, `category`, `deleted`) VALUES
+(1, 1, 2, '2016-05-16 17:10:00', 'Group A', 0),
+(2, 3, 4, '2016-05-16 18:45:00', 'Group B', 0),
+(3, 1, 3, '2016-05-16 18:50:00', 'Group C', 0),
+(4, 1, 1, '2016-05-16 18:50:00', 'Group C', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE IF NOT EXISTS `teams` (
+  `id` int(2) NOT NULL,
+  `image` varchar(512) COLLATE utf8_bin NOT NULL,
+  `name` varchar(128) COLLATE utf8_bin NOT NULL,
+  `deleted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -30,14 +83,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) COLLATE utf8_bin NOT NULL,
   `dateadded` datetime NOT NULL,
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `dateadded`, `deleted`) VALUES
-(1, 'Super Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', '2016-05-16 00:00:00', 0);
+(1, 'Super Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', '2016-05-16 00:00:00', 0),
+(2, 'Pronos Foot', 'pronosfoot', '5f4dcc3b5aa765d61d8327deb882cf99', '2016-05-16 18:01:00', 0);
 
 -- --------------------------------------------------------
 
@@ -51,14 +105,17 @@ CREATE TABLE IF NOT EXISTS `user_permissions` (
   `class_name` varchar(128) COLLATE utf8_bin NOT NULL,
   `function` varchar(128) COLLATE utf8_bin NOT NULL COMMENT '% value means any',
   `deleted` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Dumping data for table `user_permissions`
 --
 
 INSERT INTO `user_permissions` (`id`, `user_group`, `class_name`, `function`, `deleted`) VALUES
-(1, 'Admin_users', 'Admin_users', '%', 0);
+(1, 'Admin_users', 'Admin_users', '%', 0),
+(2, 'Admin_teams', 'Admin_teams', '%', 0),
+(3, 'Admin_groups', 'Admin_groups', '%', 0),
+(4, 'Admin_matches', 'Admin_matches', '%', 0);
 
 -- --------------------------------------------------------
 
@@ -76,6 +133,24 @@ CREATE TABLE IF NOT EXISTS `user_user_groups` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -100,15 +175,30 @@ ALTER TABLE `user_user_groups`
 --
 
 --
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `user_permissions`
 --
 ALTER TABLE `user_permissions`
-  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `user_user_groups`
 --
